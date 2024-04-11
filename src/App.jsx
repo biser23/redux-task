6f4872f4078eccc8e81639c6d327f65e21627b78
Biser Svilenov Stanev <biser@MacBook-Air-de-Biser.local>
2024-04-11 13:24:32 +0200

    ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, deleteTask } from './redux/todosSlice';
+
 const App = () => {
-  
+  const tasks = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+
+  const handleAddTask = () => {
+    const newTask = { id: tasks.length + 1, text: 'New Task' };
+    dispatch(addTask(newTask));
+  };
+
+  const handleDeleteTask = taskId => {
+    dispatch(deleteTask(taskId));
+  };
+
+  console.log(tasks);
+
   return (
-    <></>
+    <div>
+      <button onClick={handleAddTask}>Agregar Tarea</button>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => handleDeleteTask(task.id)}>Eliminar Tarea</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
